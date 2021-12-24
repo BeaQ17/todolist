75945d5fd0ecc6ee85a33fa92043f2e31a4d7c4e
BeaQ17 <beaq235@gmail.com>
2021-12-24 18:04:57 +0100

    start over

---
+++ app.js
@@ -0,0 +1,7 @@
+const date = document.getElementById("date");
+
+const clear = document.getElementById("clear");
+
+const items = document.getElementById("items");
+
+const add = document.getElementById("add_task");
