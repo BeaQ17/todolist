d23b8ca2cf90b09ae837a81a8239e07e65222707
BeaQ17 <beaq235@gmail.com>
2023-03-07 16:33:12 +0100

    check

--- app.js
+++ app.js
@@ -2,6 +2,28 @@ const date = document.getElementById("date");
 
 const clear = document.getElementById("clear");
 
-const items = document.getElementById("items");
+const list = document.getElementById("list");
 
-const add = document.getElementById("add_task");
+const add_task = document.getElementById("add_task");
+
+// const add_circle = document.getElementById("add_circle");
+
+function addItem(item){
+    const text = <li>
+                    <i class="far fa-check-circle fa-lg" job="complete"></i>
+                    <p id="item">${item}</p>
+                    <i class="fas fa-trash fa-lg" job="delete"></i>
+                </li>
+
+    list.insertAdjacentHTML("afterbegin", text);
+};
+
+document.addEventListener("keyup", function(event){
+    if(event.key == "Enter"){
+        const item = add_task.value;
+        if(item){
+            addItem(item);
+        };
+        add_task.value = "";
+    }
+});
